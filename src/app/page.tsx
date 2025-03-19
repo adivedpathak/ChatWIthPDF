84cec1bbfb2b4ad8a0bce753c1a3c974399bb3f4
aditya <aditya226vedpathak@gmail.com>
2025-03-20 01:57:01 +0530

    added clerk auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,52 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import Link from "next/link";
+import { ArrowRight, LogIn } from "lucide-react";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
+
   return (
- <div  className="bg-gray-100 h-screen flex items-center text-9xl justify-center">
-  heelo
- </div>
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+           
+           <div className="flex mt-2 ">
+            {isAuth && (
+              <Link href="/" passHref>
+                <Button className="mr-2">Go to Chat</Button>
+              </Link>
+            )}
+           </div>
+
+
+           <p className="max-w-xl mt-1 text-lg text-slate-600">
+            Join millions of students, researchers and professionals to instantly
+            answer questions and understand research with AI
+          </p>
+     
+          <div className="w-full mt-4">
+            {isAuth ? (
+              // <FileUpload />
+              <h1>file upload components</h1>
+            ) : (
+              <Link href="/sign-in">
+                <Button>
+                  Login to get Started!
+                  <LogIn className="w-4 h-4 ml-2" />
+                </Button>
+              </Link>
+            )}
+          </div>
+
+        </div>
+      </div>
+    </div>
   );
 }
